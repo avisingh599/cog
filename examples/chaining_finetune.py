f30ffb96f8948c9714c0ebf3219e931a377d237a
Avi Singh <avisingh599@gmail.com>
2020-10-14 17:13:13 -0700

    Simplify input args for finetuning script

--- examples/chaining_finetune.py
+++ examples/chaining_finetune.py
@@ -32,10 +32,6 @@ NFS_PATH = '/nfs/kun1/users/avi/doodad-output/'
 
 def experiment(variant):
 
-    # variant_json = os.path.join(variant['checkpoint_dir'], 'variant.json')
-    # with open(variant_json) as f:
-    #     load_variant = json.load(f)
-
     checkpoint_filepath = os.path.join(variant['checkpoint_dir'],
                                        'itr_{}.pkl'.format(
                                            variant['checkpoint_epoch']))
@@ -104,136 +100,44 @@ def enable_gpus(gpu_str):
 
 if __name__ == "__main__":
     # noinspection PyTypeChecker
-    variant = dict(
-        algorithm="CQL",
-        version="normal",
-        algorithm_kwargs=dict(
-            # num_epochs=100,
-            # num_eval_steps_per_epoch=50,
-            # num_trains_per_train_loop=100,
-            # num_expl_steps_per_train_loop=100,
-            # min_num_steps_before_training=100,
-            # max_path_length=10,
-            num_epochs=3000,
-            num_eval_steps_per_epoch=300,
-            num_trains_per_train_loop=1000,
-            num_expl_steps_per_train_loop=300,
-            min_num_steps_before_training=300,
-            max_path_length=30,
-            batch_size=256,
-        ),
-        trainer_kwargs=dict(
-            discount=0.99,
-            soft_target_tau=5e-3,
-            policy_lr=1E-4,
-            qf_lr=3E-4,
-            reward_scale=1,
-            use_automatic_entropy_tuning=True,
-
-            # Target nets/ policy vs Q-function update
-            policy_eval_start=10000,
-            num_qs=2,
-
-            # min Q
-            temp=1.0,
-            min_q_version=3,
-            min_q_weight=5.0,
-
-            # lagrange
-            with_lagrange=False,  # Defaults to False
-            lagrange_thresh=10.0,
-
-            # extra params
-            num_random=1,
-            max_q_backup=False,
-            deterministic_backup=False,
-        ),
-        cnn_params=dict(
-            kernel_sizes=[3, 3, 3],
-            n_channels=[16, 16, 16],
-            strides=[1, 1, 1],
-            hidden_sizes=[1024, 512, 256],
-            paddings=[1, 1, 1],
-            pool_type='max2d',
-            pool_sizes=[2, 2, 1],  # the one at the end means no pool
-            pool_strides=[2, 2, 1],
-            pool_paddings=[0, 0, 0],
-            image_augmentation=True,
-            image_augmentation_padding=4,
-        ),
-        dump_video_kwargs=dict(
-            imsize=48,
-            save_video_period=1,
-        ),
-    )
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--env", type=str, required=True)
-    parser.add_argument("--max-path-length", type=int, required=True)
-    parser.add_argument("--checkpoint-dir", type=str, default=DEFAULT_CHECKPOINT_DIR)
+    parser.add_argument("--checkpoint-dir", type=str,
+                        default=DEFAULT_CHECKPOINT_DIR)
     parser.add_argument("--checkpoint-epoch", type=int, default=520)
-    parser.add_argument("--prior-buffer", type=str, default=DEFAULT_PRIOR_BUFFER)
-    parser.add_argument("--task-buffer", type=str, default=DEFAULT_TASK_BUFFER)
-
     parser.add_argument("--gpu", default='0', type=str)
-    parser.add_argument("--min-q-weight", default=1.0, type=float,
+    parser.add_argument("--min-q-weight", default=None, type=float,
                         help="Value of alpha in CQL")
-    parser.add_argument("--use-lagrange", action="store_true", default=False)
-    parser.add_argument("--lagrange-thresh", default=5.0, type=float,
-                        help="Value of tau, used with --use-lagrange")
-    parser.add_argument("--use-positive-rew", action="store_true", default=False)
-
-    parser.add_argument("--max-q-backup", action="store_true", default=False,
-                        help="For max_{a'} backups, set this to true")
-    parser.add_argument("--no-deterministic-backup", action="store_true",
-                        default=False,
-                        help="By default, deterministic backup is used")
-    parser.add_argument("--policy-eval-start", default=10000,
-                        type=int)
-    parser.add_argument("--policy-lr", default=1e-4, type=float)
-    parser.add_argument("--min-q-version", default=3, type=int,
-                        help=("min_q_version = 3 (CQL(H)), "
-                              "version = 2 (CQL(rho))"))
-    parser.add_argument("--num-eval-per-epoch", type=int, default=5)
     parser.add_argument("--seed", default=10, type=int)
-
     args = parser.parse_args()
+
+    variant_json = os.path.join(args.checkpoint_dir, 'variant.json')
+    with open(variant_json) as f:
+        variant = json.load(f)
+
     enable_gpus(args.gpu)
-    variant['env'] = args.env
     variant['checkpoint_dir'] = args.checkpoint_dir
     variant['checkpoint_epoch'] = args.checkpoint_epoch
-
-    variant['algorithm_kwargs']['max_path_length'] = args.max_path_length
-    variant['algorithm_kwargs']['num_eval_steps_per_epoch'] = \
-        args.num_eval_per_epoch*args.max_path_length
     variant['algorithm_kwargs']['num_expl_steps_per_train_loop'] = \
-        10*args.max_path_length
+        10*variant['algorithm_kwargs']['max_path_length']
     variant['algorithm_kwargs']['min_num_steps_before_training'] = \
-        10*args.max_path_length
+        10*variant['algorithm_kwargs']['max_path_length']
 
     # For testing
+    # variant['prior_buffer'] = DEFAULT_PRIOR_BUFFER
+    # variant['task_buffer'] = DEFAULT_TASK_BUFFER
+    # variant['algorithm_kwargs']['num_eval_steps_per_epoch'] = 100
     # variant['algorithm_kwargs']['num_expl_steps_per_train_loop'] = 100
     # variant['algorithm_kwargs']['min_num_steps_before_training'] = 100
+    # variant['algorithm_kwargs']['num_trains_per_train_loop'] = 100
 
-    variant['prior_buffer'] = args.prior_buffer
-    variant['task_buffer'] = args.task_buffer
+    variant['offline_min_q_weight'] = variant['trainer_kwargs']['min_q_weight']
+    if args.min_q_weight is not None:
+        variant['trainer_kwargs']['min_q_weight'] = args.min_q_weight
 
-    variant['trainer_kwargs']['max_q_backup'] = args.max_q_backup
-    variant['trainer_kwargs']['deterministic_backup'] = \
-        not args.no_deterministic_backup
-    variant['trainer_kwargs']['min_q_weight'] = args.min_q_weight
-    variant['trainer_kwargs']['policy_lr'] = args.policy_lr
-    variant['trainer_kwargs']['min_q_version'] = args.min_q_version
-    variant['trainer_kwargs']['policy_eval_start'] = args.policy_eval_start
-    variant['trainer_kwargs']['lagrange_thresh'] = args.lagrange_thresh
-    variant['trainer_kwargs']['with_lagrange'] = args.use_lagrange
-
-    # Translate 0/1 rewards to +4/+10 rewards.
-    variant['use_positive_rew'] = args.use_positive_rew
     variant['seed'] = args.seed
 
     ptu.set_gpu_mode(True)
-    exp_prefix = 'cql-private-chaining-finetune-{}'.format(args.env)
+    exp_prefix = 'cql-private-chaining-finetune-{}'.format(variant['env'])
 
     if os.path.isdir(NFS_PATH):
         base_log_dir = NFS_PATH
