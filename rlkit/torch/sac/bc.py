925c9ac7d8dd08fb8174338b7583f7ebd423d820
Albert Yu <albertyu@berkeley.edu>
2020-10-16 10:58:46 -0700

    Delete comments and additional unused functions for BC.

--- rlkit/torch/sac/bc.py
+++ rlkit/torch/sac/bc.py
@@ -3,7 +3,6 @@ from collections import OrderedDict
 import numpy as np
 import torch
 import torch.optim as optim
-from torch import nn as nn
 
 import rlkit.torch.pytorch_util as ptu
 from rlkit.core.eval_util import create_stats_ordered_dict
@@ -15,60 +14,12 @@ class BCTrainer(TorchTrainer):
             self,
             env,
             policy,
-            # qf1,
-            # qf2,
-            # target_qf1,
-            # target_qf2,
-            # behavior_policy=None,
-            # dim_mult=1,
-
-            # discount=0.99,
-            # reward_scale=1.0,
-
             policy_lr=1e-3,
             optimizer_class=optim.Adam,
-
-            # soft_target_tau=1e-2,
-            # target_update_period=1,
-            # plotter=None,
-            # render_eval_paths=False,
-
-            use_automatic_entropy_tuning=True,
-            target_entropy=None,
-            # use_target_nets=True,
-            # num_qs=2,
-
-            # use_robot_state=False,
-            # observation_keys=("observations",),
     ):
         super().__init__()
         self.env = env
         self.policy = policy
-        # self.qf1 = qf1
-        # self.qf2 = qf2
-        # self.target_qf1 = target_qf1
-        # self.target_qf2 = target_qf2
-        # self.soft_target_tau = soft_target_tau
-        # self.target_update_period = target_update_period
-
-        self.use_automatic_entropy_tuning = use_automatic_entropy_tuning
-        if self.use_automatic_entropy_tuning:
-            if target_entropy:
-                self.target_entropy = target_entropy
-            else:
-                self.target_entropy = -np.prod(
-                    self.env.action_space.shape).item()  # heuristic value from Tuomas
-            self.log_alpha = ptu.zeros(1, requires_grad=True)
-            # self.alpha_optimizer = optimizer_class(
-            #     [self.log_alpha],
-            #     lr=3e-4,
-            # )
-
-        # self.plotter = plotter
-        # self.render_eval_paths = render_eval_paths
-
-        self.qf_criterion = nn.MSELoss()
-        self.vf_criterion = nn.MSELoss()
 
         self.policy_optimizer = optimizer_class(
             self.policy.parameters(),
@@ -77,30 +28,12 @@ class BCTrainer(TorchTrainer):
 
         self._optimizer_class = optimizer_class #for loading
 
-        # self.discount = discount
-        # self.reward_scale = reward_scale
         self.eval_statistics = OrderedDict()
         self._n_train_steps_total = 0
         self._need_to_update_eval_statistics = True
-        # self._use_target_nets = use_target_nets
 
         self._current_epoch = 0
-        # self._policy_update_ctr = 0
-        # self._num_q_update_steps = 0
         self._num_policy_update_steps = 0
-        # self._num_policy_steps = 1
-
-        # if not self._use_target_nets:
-        #     self.target_qf1 = qf1
-        #     self.target_qf2 = qf2
-
-        # self.num_qs = num_qs
-        self.discrete = False
-        # self.needs_online_flag = True
-        # self.use_robot_state = use_robot_state
-        # self.observation_keys = observation_keys
-        # if self.use_robot_state:
-            # assert len(self.observation_keys) > 1
 
     def preprocess_obs(self, obs, num_repeat, actions):
         if isinstance(obs, torch.Tensor):
@@ -119,33 +52,9 @@ class BCTrainer(TorchTrainer):
         else:
             raise NotImplementedError
 
-    # def _get_tensor_values(self, obs, actions, network=None):
-    #     obs_temp, obs_shape, num_repeat = self.preprocess_obs(
-    #         obs, None, actions)
-    #     preds = network(obs_temp, actions)
-    #     preds = preds.view(obs_shape, num_repeat, 1)
-    #     return preds
-
-    # def _get_policy_actions(self, obs, num_actions, network=None):
-    #     obs_temp, obs_shape_dim0, _ = self.preprocess_obs(
-    #         obs, num_actions, None)
-    #     new_obs_actions, _, _, new_obs_log_pi, *_ = network(
-    #         obs_temp, reparameterize=True, return_log_prob=True,
-    #     )
-    #     return new_obs_actions, new_obs_log_pi.view(
-    #         obs_shape_dim0, num_actions, 1)
-
-    def filter_dict(self, key_prefix, batch_dict):
-        assert isinstance(key_prefix, str)
-        return dict([(key, batch_dict[key]) for key in batch_dict.keys() 
-            if key.find(key_prefix) == 0])
-
     def train_from_torch(self, batch, online=False):
         self._current_epoch += 1
 
-        # if self.use_robot_state:
-        #     obs = self.filter_dict("observations", batch)
-        # else:
         obs = batch['observations']
         actions = batch['actions']
 
@@ -176,7 +85,6 @@ class BCTrainer(TorchTrainer):
             Eval should set this to None.
             This way, these statistics are only computed for one batch.
             """
-            # self.eval_statistics['Num Q Updates'] = self._num_q_update_steps
             self.eval_statistics[
                 'Num Policy Updates'] = self._num_policy_update_steps
             self.eval_statistics['Policy Loss'] = np.mean(ptu.get_numpy(
@@ -205,9 +113,4 @@ class BCTrainer(TorchTrainer):
     @property
     def networks(self):
         base_list = [self.policy]
-        #     self.qf1,
-        #     self.qf2,
-        #     self.target_qf1,
-        #     self.target_qf2,
-        # ]
         return base_list
